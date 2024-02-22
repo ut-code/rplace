f86bb9714f63dbc987577d1ac146ce37328758be
astervoid <137767097+aster-void@users.noreply.github.com>
2024-02-22 17:45:09 +0900

    gave up cors; now frontend host == backend serve

--- backend/main.ts
+++ backend/main.ts
@@ -1,9 +1,6 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
-
-app.use(cors({ origin: process.env["WEB_ORIGIN"] }));
 
 app.use(express.json());
 app.use(express.static("dist"));
--- frontend/serve.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.use(express.static("./vite-dist"));
-
-const PORT = 3100;
-app.listen(PORT);
-console.log(`|frontend| Listening on ${PORT}`);

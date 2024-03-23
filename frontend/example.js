a23efc403ceeb483817d46584f063dd1c14b7407
astree <137767097+aster-void@users.noreply.github.com>
2024-03-23 15:13:00 +0900

    complete array -> image conversion

---
+++ frontend/example.js
@@ -0,0 +1,47 @@
+function example() {
+	const width = 256, height = 256;
+
+	const image = document.getElementById("sample-image");
+	const arr = createRandomArray(width, height);
+	
+	setImageData(image, arr, width, height);
+}
+example();
+
+function createRandomArray(width, height) {
+	const arr = new Uint8ClampedArray(width * height * 4); 
+	for (let h = 0; h < height; h++) {
+		for (let w = 0; w < width; w++) {
+			const idx = (h * width + w ) * 4;
+			arr[idx] = w % 256; // Red
+			arr[idx + 1] = h % 256; // Green
+			arr[idx + 2] = idx % 256; // Blue
+			arr[idx + 3] = 255; // Alpha (transparency)
+		}
+	}
+	return arr;
+}
+
+// below is also defiled in image-array.ts, use one from that file for prod.
+function setImageData(image, arr, width, height) {
+
+	const canvas = document.createElement("canvas");
+	canvas.width = width;
+	canvas.height = height;
+
+	const ctx = canvas.getContext("2d");
+	if (!ctx) return 1;
+
+	const u8arr = Uint8ClampedArray.from(arr);
+
+	const imageData = ctx.createImageData(width, height);
+	imageData.data.set(u8arr);
+
+	ctx.putImageData(imageData, 0, 0);
+
+	const dataUri = canvas.toDataURL();
+
+	image.src=dataUri;
+
+	return 0;
+}
---
+++ frontend/image-array.ts
@@ -0,0 +1,24 @@
+type errno = number;
+function setImageData(image: HTMLImageElement, arr: Array<number>, width: number, height: number): errno {
+
+	const canvas = document.createElement("canvas");
+	canvas.width = width;
+	canvas.height = height;
+
+	const ctx = canvas.getContext("2d");
+	if (!ctx) return 1;
+
+	const u8arr = Uint8ClampedArray.from(arr);
+
+	const imageData = ctx.createImageData(width, height);
+	imageData.data.set(u8arr);
+
+	ctx.putImageData(imageData, 0, 0);
+
+	const dataUri = canvas.toDataURL();
+
+	image.src=dataUri;
+
+	return 0;
+}
+setImageData; // I'm using it later, stupid eslint

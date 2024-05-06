f5ef0355232101007ad44d49a93dac7eba6f62f3
aster <137767097+aster-void@users.noreply.github.com>
2024-05-06 18:37:39 +0900

    deleted unnecessary console.log

--- backend/main.ts
+++ backend/main.ts
@@ -47,7 +47,6 @@ const io = new Server(httpServer, {
   cookie: true,
 });
 
-console.log(VITE_BUTTON_COOLDOWN);
 const BUTTON_COOLDOWN_SECONDS = parseInt(VITE_BUTTON_COOLDOWN || "10"); // the fallback will be used in prod
 const IMAGE_WIDTH = 16;
 const IMAGE_HEIGHT = 16;
